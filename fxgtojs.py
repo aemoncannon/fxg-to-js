52462b47091f7a56d2b9adcd1e3b9bee0bcec5a9
Aemon Cannon <aemoncannon@gmail.com>
2012-11-18 19:11:18 -0500

    Working on rounding line caps..

--- fxgtojs.py
+++ fxgtojs.py
@@ -2,6 +2,7 @@ import sys
 import xml.dom.minidom
 import xml.dom
 import itertools
+import re
 
 def children(node, tag):
   for node in node.childNodes:
@@ -11,6 +12,11 @@ def children(node, tag):
 def attr(node, key, default=None):
   at = node.attributes.get(key, None)
   return at.value if at else default
+
+def attributes(node, key):
+  at = node.attributes.get(key, None)
+  if at:
+    yield at.value
 
 def line(line):
   sys.stdout.write(line + ";\n")
@@ -32,39 +38,67 @@ def group(node, sprite):
   for child in children(node, "Group"):
     child_sym = group(child, sprite)
     line("%s.add(%s)" % (sym, child_sym))
+  common_transforms(node, sym)
   return sym
 
 def path(node, sprite):
   sym = gensym()
-  data = attr(node, "data", "")
-  line("var %s = new Kinetic.Path({x:0,y:0})" % sym)
-  line("%s.setData('%s')" % (sym, data))
+  line("var %s = new Kinetic.Path({x:0,y:0,lineCap:'round'})" % sym)
+  for data in attributes(node, "data"):
+    cleaned = re.compile("[\r\n\t]").sub("", data)
+    line("%s.setData('%s')" % (sym, cleaned))
   for child in children(node, "fill"):
     fill(child, sym)
+  for child in children(node, "stroke"):
+    stroke(child, sym)
+  common_transforms(node, sym)
   return sym
+
+def common_transforms(node, sym):
+  for rotation in attributes(node, "rotation"):
+    line("%s.setRotationDeg(%s)" % (sym, rotation))
+  for x in attributes(node, "x"):
+    line("%s.setX(%s)" % (sym, x))
+  for y in attributes(node, "y"):
+    line("%s.setY(%s)" % (sym, y))
+  sx = attr(node, "scaleX", 1)
+  sy = attr(node, "scaleY", 1)
+  if sx != 1 or sy != 1:
+    line("%s.setScale(%s, %s)" % (sym, sx, sy))
+  for child in children(node, "transform"):
+    for child in children(child, "Transform"):
+      for child in children(child, "matrix"):
+        for mat in children(child, "Matrix"):
+          a = attr(mat, "a", 1)
+          b = attr(mat, "b", 0)
+          c = attr(mat, "c", 0)
+          d = attr(mat, "d", 1)
+          tx = attr(mat, "tx", 0)
+          ty = attr(mat, "ty", 0)
+          line("%s.setOffset(%s, %s)" % (sym, tx, ty))
+          #line("%s.getTransform().m = [%s, %s, %s, %s, %s, %s]" % (sym, a, b, c, d, tx, ty))
 
 def fill(node, path_sym):
   for child in children(node, "SolidColor"):
-    color = attr(child, "color", None)
-    alpha = attr(child, "alpha", None)
-    if color:
-      line("%s.setFill('%s')" % (path_sym, color))
-    if alpha:
-      line("%s.setOpacity(%s)" % (path_sym, float(alpha)))
+    line("%s.setFill('%s')" % (path_sym, attr(child, "color", "black")))
+    line("%s.setOpacity(%s)" % (path_sym, attr(child, "alpha", 1.0)))
+
+def stroke(node, path_sym):
+  for child in children(node, "SolidColorStroke"):
+    line("%s.setStroke('%s')" % (path_sym, attr(child, "color", "black")))
+    line("%s.setStrokeWidth('%s')" % (path_sym, attr(child, "weight", 1.0)))
 
 def graphic(node, sprite):
   group_sym = group(node, sprite)
   line("%s.add(%s)" % ("layer", group_sym))
 
-
-
-
 print "window.onload = function() {"
 print "var stage = new Kinetic.Stage({container: 'container',width: 600,height: 500});"
 print "var layer = new Kinetic.Layer();"
+print "layer.setScale(5,5);"
 
 #print couple.parseString("23.3 4")
-dom = xml.dom.minidom.parse("Bamboo.fxg")
+dom = xml.dom.minidom.parse("AuroraFern.fxg")
 graphic(dom.childNodes[0], None)
 
 print "stage.add(layer);"
